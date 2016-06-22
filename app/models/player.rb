49d92fd2270ca1aa5552611cc4fc15b0ddddfff7
Alyson Takai <shtakai@gmail.com>
2016-06-21 20:37:25 -0700

    load Player with team name

--- app/models/player.rb
+++ app/models/player.rb
@@ -2,4 +2,14 @@ class Player < ActiveRecord::Base
   belongs_to :team
 
   validates :first_name, :last_name, presence: true
+
+  def self.all_with_team
+    self.eager_load(:team).map{|p| {
+      id: p.id,
+      first_name: p.first_name,
+      last_name: p.last_name,
+      team_id: p.team_id,
+      team_name: p.team ? p.team.name : nil
+    }}
+  end
 end
